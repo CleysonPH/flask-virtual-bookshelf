fa073ef6c90d042b6984f923e0375f6474322668
Cleyson Lima <cleysonph@gmail.com>
2019-06-01 20:44:00 -0300

    Add book list page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+from flask import render_template
+
 from app import app
+from app.models import Book
 
 
 @app.route('/')
 def home():
-    return "It's works!!!"
+    title = 'Lista de Livros'
+    books = Book.query.order_by(Book.title).all()
+
+    return render_template('home.html', title=title, books=books)
