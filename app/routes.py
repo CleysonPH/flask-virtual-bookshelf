7157ae2a73c8d53716dcb72ee290a81d8bc28e4b
Cleyson Lima <cleysonph@gmail.com>
2019-06-03 20:22:12 -0300

    Implemented Edit Author and Remove Author

--- app/routes.py
+++ app/routes.py
@@ -104,6 +104,14 @@ def book_delete(id):
     return redirect(url_for('home'))
 
 
+@app.route('/author/list')
+def author_list():
+    title = 'Lista de Autores'
+    authors = Author.query.order_by(Author.name).all()
+
+    return render_template('author/author_list.html', title=title, authors=authors)
+
+
 @app.route('/author/add', methods=['GET', 'POST'])
 def author_add():
     author = 0
@@ -122,3 +130,31 @@ def author_add():
 
     flash("Autor cadastrado com sucesso!")
     return redirect(url_for('author_add'))
+
+
+@app.route('/author/edit/<int:id>', methods=['GET', 'POST'])
+def author_edit(id):
+    author = Author.query.get_or_404(id)
+    if request.method == 'GET':
+        title = 'Editar Autor'
+        return render_template('author/author_form.html', title=title, author=author)
+     
+    author.name=request.form.get('name')
+    author.img_url=request.form.get('img_url')
+    author.description=request.form.get('description')
+
+    db.session.commit()
+
+    flash("Autor editado com sucesso!")
+    return redirect(url_for('author_list'))
+
+
+@app.route('/author/remove/<int:id>')
+def author_remove(id):
+    author = Author.query.get_or_404(id)
+
+    db.session.delete(author)
+    db.session.commit()
+
+    flash("Autor deletado com sucesso!")
+    return redirect(url_for('author_list'))
